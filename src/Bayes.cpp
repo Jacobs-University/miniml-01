2be2c468dcc55496fb0723b98cc8b52aa6108018
Bonaventure DOSSOU <43601208+bonaventuredossou@users.noreply.github.com>
2021-04-01 15:46:04 +0200

    Submission solution "Bayes.cpp"

--- src/Bayes.cpp
+++ src/Bayes.cpp
@@ -1,5 +1,6 @@
 #include "Bayes.h"
 #include <numeric>
+using namespace std;
 
 CBayes::CBayes(byte nStates, word nFeatures) 
 	: m_nStates(nStates)
@@ -13,9 +14,13 @@ CBayes::CBayes(byte nStates, word nFeatures)
 
 void CBayes::addFeatureVec(const Mat & featureVector, byte gt)
 {
-	// --- PUT YOUR CODE HERE ---
-
-
+	for (int i = 0; i < m_nStates; i++) {
+		if (i == gt)
+			m_pPrior->addPoint(gt);
+	}
+	// the run gives: 17.2% 0.4% 59.5% 9.9% 13.0% 0.0%
+	// The class that hasn't been represented in the training data is the class "car" with probability 0
+	
 
 	for (word f = 0; f < m_nFeatures; f++) {
 		byte feature = featureVector.at<byte>(f, 0);
@@ -23,17 +28,24 @@ void CBayes::addFeatureVec(const Mat & featureVector, byte gt)
 	}
 }
 
-Mat CBayes::getPotentials(const Mat & featureVector) const
+Mat CBayes::getPotentials(const Mat& featureVector) const
 {
 	Mat res(m_nStates, 1, CV_32FC1);
-	
+
 	for (byte s = 0; s < m_nStates; s++)
 		res.at<float>(s, 0) = static_cast<float>(m_pPrior->getDensity(s));
-	
+
 	// --- PUT YOUR CODE HERE ---
 
-
-	
+	for (byte s = 0; s < m_nStates; s++) {
+		for (word f = 0; f < m_nFeatures; f++) {
+			byte feature = featureVector.at<byte>(f, 0);
+			if (!m_vPDF[f * m_nStates + s]->isEstimated())
+				res.at<float>(s, 0) = 0;
+			else res.at<float>(s, 0) = res.at<float>(s, 0) * m_vPDF[f * m_nStates + s]->getDensity(feature);
+			
+		}
+	}
 	return res;
 }
 
